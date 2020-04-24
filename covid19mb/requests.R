d60a1c9d31d92072d1da06aca715bca4b18494de
SamMorrissette <samuel.morrissette01@gmail.com>
2020-04-23 21:31:26 -0500

    Add new webscraping

--- covid19mb/requests.R
+++ covid19mb/requests.R
@@ -38,84 +38,86 @@ fullData <- data.frame(Age=categories,
                        FemaleCount=females)
 write.csv(fullData,'/srv/shiny-server/covid19mb/AgeGenderData.csv',row.names=FALSE)
 
-#TABLE
+#REGIONAL
 mainpage <- read_html('https://www.gov.mb.ca/covid19/updates/index.html')
-tbls_ls <- html_nodes(mainpage, "table") %>% 
-  html_table(fill = TRUE,header=TRUE)
-fullTable <- tbls_ls[[1]]
-HR_UID <- c("IERHA","NRHA","PMH","SHR","WRHA","WRHA-CH")
-Region <- c("Interlake-Eastern Regional Health Authority",
+url <- paste0('https://manitoba.ca/health/publichealth/public_app/By_RHA_',today,'.html')
+webpage <- readURL(url)
+
+data <- webpage %>% 
+  html_nodes('script') %>% 
+  html_text() %>% 
+  grep(pattern = "x", value = TRUE) %>% 
+  fromJSON()
+confirmed <- data$x$data$x[[1]]
+presumptive <- data$x$data$x[[2]]
+confirmed.cat <- data$x$data$y[[1]]
+presumptive.cat <- data$x$data$y[[2]]
+confirmed.df <- data.frame(Region=confirmed.cat,
+                           Confirmed=confirmed)
+presumptive.df <- data.frame(Region=presumptive.cat,
+                          Presumptive=presumptive)
+full.df <- merge(confirmed.df,presumptive.df,all.x=TRUE)
+full.df$Presumptive[is.na(full.df$Presumptive)] <- 0
+ch.df <- data.frame(Region="Winnipeg Regional Health Authority (Churchill)",
+                    Confirmed=0,
+                    Presumptive=0)
+full.df <- rbind(full.df,ch.df)
+
+full.df$HR_UID <- c("IERHA","NRHA","PMH","SHR","WRHA","WRHA-CH")
+full.df$Region <- c("Interlake-Eastern Regional Health Authority",
             "Northern Regional Health Authority",
             "Prairie Mountain Health",
             "Southern Health-Sante Sud",
             "Winnipeg Regional Health Authority",
             "Winnipeg Regional Health Authority (Churchill)")
+full.df$Total <- full.df$Confirmed+full.df$Presumptive
 
-#Add zeros for churchill
-fullTable <- fullTable[1:5,]
-Presumptive <- c(fullTable$`Probable Positive **`,0)
-Confirmed <- c(fullTable$`Confirmed Positive *`,0)
-Total <- c(fullTable$`Total Cases`,0)
+write.csv(full.df,'/srv/shiny-server/covid19mb/CaseData.csv',row.names=FALSE)
 
-fullDataFrame <- data.frame(HR_UID=HR_UID,
-                            Region=Region,
-                            Presumptive=Presumptive,
-                            Confirmed=Confirmed,
-                            Total=Total)
-write.csv(fullDataFrame,'/srv/shiny-server/covid19mb/CaseData.csv',row.names=FALSE)
-
+#EPICURVE
 epiCurve <- read.csv('/srv/shiny-server/covid19mb/epicurve.csv')
 epiCurve$date <- as.Date(epiCurve$date)
 
-#Check to see if the last updated date is the same as the last date in the csv file.
-#If not, then add the last updated date to the last row along with the "total" row from the table
-#New cases will be the new row cumulative minus the last row cumulative.
-updatedDate <- html_nodes(mainpage,xpath="//h3[contains(., 'Cases and Risk of COVID')]/following-sibling::p") %>%
-  grep(pattern="Last updated:",value=TRUE)
-updatedDate <- updatedDate[1] %>% 
-  str_extract(pattern='(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\\s+\\d{1,2},\\s+\\d{4}') %>%
-  as.Date(format="%B %d, %Y")
 lastDate <- tail(epiCurve$date,1)
-if (updatedDate != lastDate) {
-  newCum <- tbls_ls[[1]]$`Total Cases`[6]
-  newNew <- newCum-tail(epiCurve$cumulative,1)
-  newRow <- data.frame(date=updatedDate,
+newDate <- lastDate+1
+
+newCum <- sum(full.df$Total)
+newNew <- newCum-tail(epiCurve$cumulative,1)
+newRow <- data.frame(date=newDate,
                        new=newNew,
                        cumulative=newCum)
-  newData <- rbind(epiCurve,newRow)
-  write.csv(newData,'/srv/shiny-server/covid19mb/epicurve.csv',row.names = FALSE)
-}
+newData <- rbind(epiCurve,newRow)
+write.csv(newData,'/srv/shiny-server/covid19mb/epicurve.csv',row.names = FALSE)
 
-#Number of tests
-numTests <- html_nodes(mainpage,"p") %>%
-  grep(pattern="tests for COVID",value=TRUE) %>%
-  str_extract(pattern="(\\d{0,3},)?(\\d{3},)?\\d{0,3}(?=\\s+tests)")
-numTests <- as.numeric(gsub("\\,", "", numTests))
+#TESTS COMPLETED
+numTests <- html_nodes(mainpage,xpath="//*[(@id = 'test-button')]//h1") %>%
+  html_text()
+numTests <- as.numeric(gsub("\\,","",numTests))
 
 testsCompleted <- read.csv('/srv/shiny-server/covid19mb/TestsCompleted.csv')
+newTests <- numTests - tail(testsCompleted$TestsCompleted,1)
 testsCompleted$Date <- as.Date(testsCompleted$Date,format="%Y-%m-%d")
-newData <- data.frame()
+newRow <- data.frame(Date=Sys.Date(),
+                     TestsCompleted=numTests,
+                     newTests=newTests)
+newData <- rbind(testsCompleted,newRow)
 
-if (numTests != testsCompleted$TestsCompleted[nrow(testsCompleted)]) {
-  newTests <- numTests-testsCompleted$TestsCompleted[nrow(testsCompleted)]
-  newData <- data.frame(Date=c(testsCompleted$Date,
-                               testsCompleted$Date[nrow(testsCompleted)]+1),
-                        TestsCompleted=c(testsCompleted$TestsCompleted,
-                                         numTests),
-                        newTests=c(testsCompleted$newTests,newTests))
-  write.csv(newData,'/srv/shiny-server/covid19mb/TestsCompleted.csv',row.names=FALSE)
-}
+write.csv(newData,'/srv/shiny-server/covid19mb/TestsCompleted.csv',row.names=FALSE)
+
 
 #DIR
-recovered <- html_nodes(mainpage,"li") %>% 
-  grep(pattern="recovered from COVID-19",value=TRUE) %>%
-  str_extract("(\\d{0,3},)?(\\d{3},)?\\d{0,3}(?=\\s+individuals)")
-recovered <- as.numeric(gsub("\\,", "", recovered))
-deaths <- tbls_ls[[1]]$Deaths[6]
-infected <- tbls_ls[[1]]$`Total Cases`[6] - (recovered+deaths)
-DIR <- data.frame(Status=c("Infected", "Recovered", "Deaths", "Tested"),
-                  Number=c(infected,recovered,deaths,numTests))
-write.csv(DIR,"/srv/shiny-server/covid19mb/Recovered.csv",row.names=FALSE)
+numRecovered <- html_nodes(mainpage,xpath="//*[(@id = 'recovered-button')]//h1") %>%
+  html_text()
+numRecovered <- as.numeric(gsub("\\,","",numRecovered))
 
-text <- paste("Last updated:",Sys.time())
-write.table(text,"/srv/shiny-server/covid19mb/LastUpdated.txt")
+numDeaths <- html_nodes(mainpage,xpath="//*[(@id = 'deaths-button')]//h1") %>%
+  html_text()
+numDeaths <- as.numeric(gsub("\\,","",numDeaths))
+
+numinfected <- html_nodes(mainpage,xpath="//*[(@id = 'active-button')]//h1") %>%
+  html_text()
+numInfected <- as.numeric(gsub("\\,","",numInfected))
+
+DIR <- data.frame(Status=c("Infected", "Recovered", "Deaths", "Tested"),
+                  Number=c(numInfected,numRecovered,numDeaths,numTests))
+write.csv(DIR,"/srv/shiny-server/covid19mb/Recovered.csv",row.names=FALSE)
