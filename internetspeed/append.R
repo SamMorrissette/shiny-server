db50b4db5d9b257dd70fe68c6d170215c621a7b4
SamMorrissette <samuel.morrissette01@gmail.com>
2020-09-16 17:06:35 -0500

    fix

--- internetspeed/append.R
+++ internetspeed/append.R
@@ -1,5 +1,5 @@
-last_test <- read_csv('last_test.csv')
-all_test <- read_csv('all_test.csv')
+last_test <- read_csv('/srv/shiny-server/internetspeed/last_test.csv')
+all_test <- read_csv('/srv/shiny-server/internetspeed/all_test.csv')
 if (last_test$time != all_test[nrow(all_test),]$time) {
   write.table(last_test,'/srv/shiny-server/internetspeed/all_test.csv', 
               sep=',', 
