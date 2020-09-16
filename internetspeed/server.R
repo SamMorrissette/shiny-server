b7efb0d647632d4da7a434650154d341b135bd69
SamMorrissette <samuel.morrissette01@gmail.com>
2020-09-16 17:05:45 -0500

    fix

--- internetspeed/server.R
+++ internetspeed/server.R
@@ -5,12 +5,6 @@ library(tidyverse)
 shinyServer(function(input, output) {
   
   #Get last test and append to all tests
-  last_test <- read_csv('/srv/shiny-server/internetspeed/last_test.csv')
-  write.table(last_test,'/srv/shiny-server/internetspeed/all_test.csv', 
-              sep=',', 
-              append=TRUE, 
-              col.names = FALSE, 
-              row.names=FALSE)
   
   #All tests is all speedtests from start to now
   all_test <- read_csv('/srv/shiny-server/internetspeed/all_test.csv')
