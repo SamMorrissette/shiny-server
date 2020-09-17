3dfd8df96fde46dcb72dd316a9e667dea97d84db
SamMorrissette <samuel.morrissette01@gmail.com>
2020-09-16 21:43:49 -0500

    test

--- internetspeed/server.R
+++ internetspeed/server.R
@@ -5,30 +5,23 @@ library(tidyverse)
 shinyServer(function(input, output) {
   #All tests is all speedtests from start to now
   all_test <- read_csv('/srv/shiny-server/internetspeed/all_test.csv')
-  vals <- reactiveValues()
-  vals <- reactiveValues()
-  observe({
-    vals$min_date <- input$dateRange[1]
-    vals$max_date <- input$dateRange[2]
+  
+  filtered <- reactive({
+    all_test %>%
+      filter(time >= input$dateRange[1] & time <= input$dateRange[2])
   })
   
   output$download <- renderPlot({
-    filtered <- all_test %>%
-      filter(time >= vals$min_date & time <= vals$max_date)
     ggplot(filtered, aes(x = time, y = download)) +
       geom_point() +
       geom_line()
     })
   output$upload <- renderPlot({
-    filtered <- all_test %>%
-      filter(time >= vals$min_date & time <=  vals$max_date)
     ggplot(filtered, aes(x = time, y = upload)) +
       geom_point() +
       geom_line()
   })
   output$ping <- renderPlot({
-    filtered <- all_test %>%
-      filter(time >= vals$min_date & time <= vals$max_date)
     ggplot(filtered, aes(x = time, y = ping)) +
       geom_point() +
       geom_line()
