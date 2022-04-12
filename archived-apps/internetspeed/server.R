f3afe7a71bce9b8c05ff89775601e2dbf8952355
SamMorrissette <samuel.morrissette01@gmail.com>
2022-04-11 22:09:50 -0500

    New server

--- archived-apps/internetspeed/server.R
+++ archived-apps/internetspeed/server.R
@@ -1,11 +1,10 @@
 library(shiny)
 library(tidyverse)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   #All tests is all speedtests from start to now
   all_test <- read_csv('/home/sam/SpeedTesting/all_test.csv')
-  
+  all_test$download <- all_test$download / (1024*1024)
   
   filtered <- reactive({
     all_test %>%
@@ -17,16 +16,19 @@ shinyServer(function(input, output) {
   output$download <- renderPlot({
     ggplot(filtered(), aes(x = time, y = download)) +
       geom_point() +
-      geom_line()
+      geom_line() + 
+      theme_light()
     })
   output$upload <- renderPlot({
     ggplot(filtered(), aes(x = time, y = upload)) +
       geom_point() +
-      geom_line()
+      geom_line()  + 
+      theme_light()
   })
   output$ping <- renderPlot({
     ggplot(filtered(), aes(x = time, y = ping)) +
       geom_point() +
-      geom_line()
+      geom_line() + 
+      theme_light()
   })
 })
